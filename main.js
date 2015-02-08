00226b4752dc90b6baf2abae666054bd6280ab76
Scott Little <uber5001@gmail.com>
2015-02-08 18:53:38 -0500

    add filmstrip polymer element

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener("polymer-ready", function() {
+	var universe = getUniverse();
+	var q = 1;
+	setInterval( function() {
+		addFrame("data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7");
+		universe.frames[universe.frames.length-1].duration = q++
+	}, 333);
+});
