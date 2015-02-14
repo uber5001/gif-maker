111490b730daf5a2e155239cc3f5127abfc9909f
Scott Little <uber5001@gmail.com>
2015-02-14 16:17:50 -0500

    add horizontal scroll

--- controllers.js
+++ controllers.js
@@ -6,9 +6,11 @@ gifMakerApp.controller('FilmstripCtrl', function($scope) {
 	$scope.moveFrame = moveFrame;
 	var universe = getUniverse();
 	var q = 1;
-	setInterval( function() {
+	function addContinuous() {
 		addFrame("data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7");
 		universe.frames[universe.frames.length-1].duration = q++;
 		$scope.$apply();
-	}, 2000);
+		setTimeout(addContinuous, 100*q);
+	}
+	addContinuous();
 });
