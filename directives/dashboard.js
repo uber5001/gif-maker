d2c9cc4bb4934517208d06dec6ab4a69f81db9c5
kingshu <kmedhi@purdue.edu>
2015-03-06 21:59:02 +0000

    dashboard validate number

--- directives/dashboard.js
+++ directives/dashboard.js
@@ -5,6 +5,17 @@ gifMakerApp.directive('dashboard', function() {
 		templateUrl: 'directives/dashboard.html'
 	}
 });
+
 gifMakerApp.controller('DashboardCtrl', function($scope) {
-	
+	$scope.validateNumber = function($event) {
+		var numRegex = /^\d*(\.\d+)?$/ ;
+		console.log($event.target.value);
+		if(numRegex.test($event.target.value)){
+			universe[$event.target.name] = $event.target.value;
+		}
+		else {
+			alert("Please enter a valid number");
+			$event.target.value = universe.$event.target.name;
+		}
+	}
 });
